f201e285647e793363345cf33e07b41f6219b097
Anthony <luo18chao@gmail.com>
2018-09-11 17:46:01 +0800

    restService controller added

--- src/main/java/com/example/rest/webservices/restfulwebservices/helloWorldController.java
+++ src/main/java/com/example/rest/webservices/restfulwebservices/helloWorldController.java
@@ -1,8 +1,7 @@
 package com.example.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
@@ -12,8 +11,19 @@ public class helloWorldController {
 	//GET
 	//URI - /hello-world
 	//method -"Hello World"
-	@GetMapping(path="/hello-world")
+	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World";
 	}
+	
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello world");
+	}
+	
+	@GetMapping(path="/hello-world/path-vaiable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello world,%s",name));
+	}
+	
 }
